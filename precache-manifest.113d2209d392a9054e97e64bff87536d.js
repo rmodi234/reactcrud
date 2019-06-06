2b4acd7f4c56b07a0897f6750325934e49ff7d6c
unknown <rahul.modi@neologicx.com>
2019-06-06 11:11:37 +0530

    Updates

---
+++ precache-manifest.113d2209d392a9054e97e64bff87536d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa0052806f98d4b1cdd0b1bb8819ecb",
+    "url": "/reactcrud/index.html"
+  },
+  {
+    "revision": "52aea496eac5a205056f",
+    "url": "/reactcrud/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e92556d96893d5f4c68d",
+    "url": "/reactcrud/static/js/2.38c21310.chunk.js"
+  },
+  {
+    "revision": "52aea496eac5a205056f",
+    "url": "/reactcrud/static/js/main.1f4b9223.chunk.js"
+  },
+  {
+    "revision": "d30751f0a629ab9c9259",
+    "url": "/reactcrud/static/js/runtime~main.f9902a9a.js"
+  }
+]);
